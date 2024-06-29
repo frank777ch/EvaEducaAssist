fe2441d7ea5f6dd16da8aa5c51e33e93867910b0
frank777ch <150204523+frank777ch@users.noreply.github.com>
2024-06-29 18:47:22 -0500

    Tamaño de avatar

--- public/js/avatar.js
+++ public/js/avatar.js
@@ -3,9 +3,6 @@ const {
     live2d: { Live2DModel },
 } = PIXI;
 
-// Kalidokit provides a simple easing function
-// (linear interpolation) used for animation smoothness
-// you can use a more advanced easing function if you want
 // Url to Live2D
 const modelUrl = "../eva/EvaEduca.model3.json";
 
@@ -20,16 +17,15 @@ const videoElement = document.querySelector(".input_video"),
         view: document.getElementById("live2d"),
         autoStart: true,
         backgroundAlpha: 0,
-        backgroundColor: 0xffffff,
         resizeTo: window,
     });
 
     // load live2d model
     currentModel = await Live2DModel.from(modelUrl, { autoInteract: false });
-    currentModel.scale.set(0.5);
+    currentModel.scale.set(0.6); // Ajusta la escala según sea necesario
     currentModel.interactive = true;
     currentModel.anchor.set(0.5, 0.5);
-    currentModel.position.set(window.innerWidth * 0.5, window.innerHeight * 1);
+    currentModel.position.set(window.innerWidth / 2, window.innerHeight / 2);
 
     // Add events to drag model
     currentModel.on("pointerdown", (e) => {
@@ -46,34 +42,19 @@ const videoElement = document.querySelector(".input_video"),
         }
     });
 
-    // Add mousewheel events to scale model
-    // document.querySelector("#live2d").addEventListener("wheel", (e) => {
-    //     e.preventDefault();
-    //     currentModel.scale.set(clamp(currentModel.scale.x + event.deltaY * -0.001, -0.5, 10));
-    // });
-
-    // add live2d model to stage
+    // Add live2d model to stage
     app.stage.addChild(currentModel);
     window.currentModel = currentModel;
 })();
 
-
-window.mover_boca = (x,y, lerpAmount = 0.7) => {
-
+window.mover_boca = (x, y, lerpAmount = 0.7) => {
     const coreModel = currentModel.internalModel.coreModel;
 
     currentModel.internalModel.motionManager.update = (...args) => {
-
-        let mouth_value = (coreModel.getParameterValueById("ParamMouthOpenY")-y)*0.3;
+        let mouth_value = (coreModel.getParameterValueById("ParamMouthOpenY") - y) * 0.3;
         coreModel.setParameterValueById(
             "ParamMouthOpenY",
-            //lerp(y, coreModel.getParameterValueById("ParamMouthOpenY"), 0.3)
-            (coreModel.getParameterValueById("ParamMouthOpenY") - y)*0.3 + y
+            (coreModel.getParameterValueById("ParamMouthOpenY") - y) * 0.3 + y
         );
-        // Adding 0.3 to ParamMouthForm to make default more of a "smile"
-        // coreModel.setParameterValueById(
-        //     "ParamMouthForm",
-        //     0.3 + (coreModel.getParameterValueById("ParamMouthOpenY") - x)*0.3 + x
-        // );
     };
 };
